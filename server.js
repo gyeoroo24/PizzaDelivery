e37839fed4f1a50544a94a821c7d41cb3455a5a9
Dinky <gyeoroo24@gmail.com>
2022-02-21 00:46:11 +0530

    Created Login Register and Cart Page

--- server.js
+++ server.js
@@ -4,14 +4,32 @@ const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.render('home');
-})
-
+//set Template Engine------Make sure that it is always above routes as we
+//inject the body in the layout.ejs file
 app.use(expressLayout);
 
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+
+//Assets ----using static middleware to apply css
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT , () => {
     console.log(`Listening on port  ${PORT}`);
